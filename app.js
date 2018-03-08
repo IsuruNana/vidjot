206e7e698691677d45e57091c8acce0554f890ce
IsuruNana <isurunanayakkara505@gmail.com>
2018-03-07 21:42:03 -0600

    Adding forms

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+//Connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev')
+.then(res => {
+    console.log('MongoDB Connected');
+})
+.catch(err => {
+    console.log(err);
+});
+
+//Load idea model
+require('./models/Idea');
+const idea = mongoose.model('ideas');
 
 /*//How middleware works
 app.use((req, res, next) => {
@@ -14,7 +31,13 @@ app.use((req, res, next) => {
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
-app.set('view engine', 'handlebars')
+app.set('view engine', 'handlebars');
+
+//Body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
 
 //Index route
 app.get('/', (req, res) => {
@@ -27,6 +50,18 @@ app.get('/', (req, res) => {
 //Index route
 app.get('/about', (req, res) => {
     res.render('about');
+});
+
+
+//Add idea form
+app.get('/ideas/add', (req, res) => {
+    res.render('ideas/add');
+});
+
+//Process form
+app.post('/ideas', (req, res) => {
+    console.log(req.body);
+    res.send('ok')
 });
 
 const port = 5000;
