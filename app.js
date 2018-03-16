92f7b16bbfaaad6dddfe76c86beda7b2797d5847
IsuruNana <isurunanayakkara505@gmail.com>
2018-03-16 12:42:09 -0500

    FInished setting up UI and CRUD functionalities

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
 
+//Main app
 const app = express();
+
+
 
 //Map global promise - get rid of warning
 mongoose.Promise = global.Promise;
@@ -37,6 +43,26 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//Method ovveride middleware
+app.use(methodOverride('_method'));
+
+//Express session middleware
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+//Flash middleware
+app.use(flash());
+
+//Global vars
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
 
 
 //Index route
@@ -47,15 +73,40 @@ app.get('/', (req, res) => {
     });
 });
 
-//Index route
+//About route
 app.get('/about', (req, res) => {
     res.render('about');
+});
+
+//Idea route/index page
+app.get('/ideas', (req, res) => {
+    //Find everything
+    idea.find({})
+        .sort({date:'desc'})
+        .then(ideas => {
+            res.render('ideas/index', {
+                ideas: ideas
+            });
+        });
+
 });
 
 
 //Add idea form
 app.get('/ideas/add', (req, res) => {
     res.render('ideas/add');
+});
+
+//Edit idea form
+app.get('/ideas/edit/:id', (req, res) => {
+    idea.findOne({
+        _id: req.params.id
+    })
+    .then(idea => {
+        res.render('ideas/edit', {
+            idea: idea
+        })
+    });
 });
 
 //Process form
@@ -77,8 +128,48 @@ app.post('/ideas', (req, res) => {
         });
     }
     else {
-        res.send('passed');
+        //res.send('passed');
+        const newUser = {
+            title: req.body.title,
+            details: req.body.details
+        }
+        new idea(newUser)
+            .save()
+            .then(idea => {
+                req.flash('success_msg', 'Video idea added');
+                res.redirect('/ideas');
+            })
     }
+});
+
+//Edit Form Process
+app.put('/ideas/:id', (req, res) => {
+    //res.send('PUT');
+    idea.findOne({
+        _id: req.params.id 
+    })
+    .then(idea => {
+        //new values
+        idea.title = req.body.title;
+        idea.details = req.body.details;
+
+        idea.save()
+            .then(idea => {
+                res.redirect('/ideas');
+            });
+    });
+});
+
+//Edit Form Process
+app.delete('/ideas/:id', (req, res) => {
+    //res.send('DELETE');
+    idea.remove({
+        _id: req.params.id
+    })
+    .then(() => {
+        req.flash('success_msg', 'Video idea removed');
+        res.redirect('/ideas');
+    })
 });
 
 const port = 5000;
